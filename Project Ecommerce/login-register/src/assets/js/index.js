de5e81dc5a9eccd4fc03878a95d3818ad74e009d
HenzoVz <murilohezo@gmail.com>
2019-10-03 09:40:10 -0300

    update

--- Project Ecommerce/login-register/src/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-function mostrarMais(desc, botaoMais){
-    document.getElementById(desc).style.display = 'inline';
-    document.getElementById(botaoMais).style.display = 'none';
-}
-function mostrarMenos(desc, botaoMais){
-    document.getElementById(desc).style.display = 'none';
-    document.getElementById(botaoMais).style.display = 'inline';
-}
